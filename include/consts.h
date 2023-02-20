41646ed54be5a6df47a8b4c9aba03359b76180fc
Paul Zupan <paul.zupan.13@gmail.com>
2023-02-20 12:50:19 -0800

    Initial commit

---
+++ include/consts.h
@@ -0,0 +1,7 @@
+#ifndef consts_h
+#define consts_h
+
+const extern int SCREEN_WIDTH;
+const extern int SCREEN_HEIGHT;
+
+#endif // consts_h
---
+++ src/ground.c
@@ -0,0 +1,28 @@
+#include "ground.h"
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+#include "consts.h"
+
+const int GROUND_SIZE = 100;
+
+SDL_Texture *ground_texture;
+
+void smb_ground_load(SDL_Renderer *renderer) {
+  ground_texture = IMG_LoadTexture(renderer, "../images/ground.png");
+}
+
+int smb_ground_draw(SDL_Renderer *renderer) {
+  int result;
+  for (int x = 0; x < SCREEN_WIDTH; x += GROUND_SIZE) {
+    SDL_Rect ground_rect =
+      {x, SCREEN_HEIGHT - GROUND_SIZE, GROUND_SIZE, GROUND_SIZE};
+    result = SDL_RenderCopy(renderer, ground_texture, NULL, &ground_rect);
+  }
+  return result;
+}
+
+void smb_ground_unload(void) {
+  SDL_DestroyTexture(ground_texture);
+}
